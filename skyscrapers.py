7f201876ffca286cabe4f155435df38ea5499128
Yurii Bodakovskyi <yurii.bodakovskyi@ucu.edu.ua>
2021-02-07 18:51:40 +0200

    Added documentation to module

--- skyscrapers.py
+++ skyscrapers.py
@@ -1,3 +1,48 @@
+'''
+This module checks a board of skyscrapers game
+ and returns True if it is correct and False if isn't
+
+Github: https://github.com/YuraBD/Skyscrapers
+
+This module has 7 functions:
+
+1) read_input
+Read game board file from path.
+Return list of str.
+
+2) left_to_right_check
+Check row-wise visibility from left to right.
+Return True if number of building from the left-most hint is visible looking to the right,
+wFalse otherwise.
+
+input_line - representing board row.
+pivot - number on the left-most hint of the input_line.
+
+3) check_not_finished_board
+Check if skyscraper board is not finished, i.e., '?' present on the game board.
+Return True if finished, False otherwise.
+
+4) check_uniqueness_in_rows
+Check buildings of unique height in each row.
+Return True if buildings in a row have unique length, False otherwise.
+
+5) check_horizontal_visibility
+Check row-wise visibility (left-right and vice versa)
+Return True if all horizontal hints are satisfiable,
+ i.e., for line 412453* , hint is 4, and 1245 are the four buildings
+  wthat could be observed from the hint looking to the right.
+
+6) check_columns
+Check column-wise compliance of the board for uniqueness (buildings of unique height) and visibility (top-bottom and vice versa).
+Same as for horizontal cases, but aggregated in one function for vertical case, i.e. columns.
+
+7) check_skyscrapers
+Main function to check the status of skyscraper game board.
+Return True if the board status is compliant with the rules,
+False otherwise.
+'''
+
+
 def read_input(path: str) -> list:
     """
     Read game board file from path.
